15ec48db6b23f853b42dc8c809bb52e2e0095e88
IMDOOWU <98314092+IMDOOWU@users.noreply.github.com>
2022-01-25 22:15:35 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -348,9 +348,9 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
+        if (score <= 49) return '菜就多练练';
+        if (score <= 99) return '是衡中学生';
+        if (score <= 149) return '衡中学霸！';
         if (score <= 199) return '您';
         return '人？';
     }
